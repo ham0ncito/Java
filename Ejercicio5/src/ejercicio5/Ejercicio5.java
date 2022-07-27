e952fee53ef6613db06368c2b7dab0febce37200
ham0ncito <lil_ham0n@hotmail.com>
2022-07-26 22:34:03 -0600

    suo

--- Ejercicio5/src/ejercicio5/Ejercicio5.java
+++ Ejercicio5/src/ejercicio5/Ejercicio5.java
@@ -5,17 +5,80 @@
  */
 package ejercicio5;
 
-/**
- *
- * @author HP
- */
+import java.util.Scanner;
+
+
 public class Ejercicio5 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+   
+    public static void main(String[] args) 
+    {
+        boolean opcionIncorrecta; 
+        do
+        {
+        opcionIncorrecta = false; 
+        Scanner scan = new Scanner(System.in); 
+        System.out.println(" Calculadora  \n Elija una opcion \n 1. Area de un triangulo "
+                + "\n 2. Area Cuadrado "
+                + "\n 3. Area de un circulo \n 4. Area de un rectangulo \n 5. Salir. \n Opcion: "); 
+        switch (scan.nextInt())
+        {
+            case 1: 
+                    System.out.println(" \n Ingrese la altura del triangulo"); 
+                    float h = scan.nextFloat(); 
+                    System.out.println(" \n Ingrese la base del triangulo"); 
+                    float b = scan.nextFloat();
+                    System.out.println(" El resultado es: " + Ejercicio5.areaTriangulo(b, h) + " unidades cuadradas" );
+                break; 
+            case 2: 
+                System.out.println(" \n Ingrese el valor de un lado del cuadrado"); 
+                 float ladoCuadrado = scan.nextFloat();
+                 System.out.println(" El resultado es: " + Ejercicio5.areaCuadrado(ladoCuadrado) + " unidades cuadradas" );
+                break; 
+            case 3: 
+                System.out.println(" \n Ingrese el valor del radio del circulo"); 
+                 float radio = scan.nextFloat();
+                 System.out.println(" El resultado es: " + Ejercicio5.areaCirculo(radio) + " unidades cuadradas" );
+                    break; 
+            case 4: 
+                 System.out.println(" \n Ingrese el largo del rectangulo"); 
+                    float largo = scan.nextFloat();
+                    System.out.println(" \n Ingrese el ancho del rectangulo"); 
+                    float ancho = scan.nextFloat();
+                    System.out.println(" El resultado es: " + Ejercicio5.areaTriangulo(largo, ancho) + " unidades cuadradas" );
+                break; 
+            case 5:
+                System.exit(0); 
+                break; 
+            default: 
+                System.out.println(" Opcion incorrecta ");
+                opcionIncorrecta = true; 
+                break; 
+                
+          
+        }
+          System.out.println("\n\t ¿Desea realizar otra operacion?   [s/n]"); 
+            if (scan.next().equals("s"))
+            {
+                opcionIncorrecta = true; 
+            }
+        } while (opcionIncorrecta); 
     }
+    public static float areaTriangulo (float base, float altura)
+    {
+        return (float)(base * altura * 0.5); 
+    }
+    public static float areaCuadrado(float lado)
+      {
+        return lado * lado; 
+    }      
+     public static float areaCirculo(float radio)
+      {
+        return (float)(Math.PI* (radio* radio)); 
+      }     
+      public static float areaRectangulo(float largo, float ancho)
+      {
+        return (float)(largo* ancho); 
+    }     
     
 }
